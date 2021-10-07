8ac1bcc478f0ca372cbac9ffc6cc0ebafabe4b68
omv0102 <omv98@outlook.com>
2021-10-08 06:12:08 +0700

    Реализовал функции класса FTPClient

---
+++ FtpClient/FTPClient.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnterpriseDT.Net.Ftp;
+
+namespace FtpClient
+{
+    public class FTPClient
+    {
+        private FTPConnection ftpConnection;
+
+        FTPClient(string host, string user, string password)
+        {
+            this.ftpConnection = new FTPConnection();
+            this.ftpConnection.ServerAddress = host;
+            this.ftpConnection.UserName = user;
+            this.ftpConnection.Password = password;
+            ftpConnection.AutoLogin = true;
+        }
+
+        public string ServerAddress
+        {
+            get
+            {
+                return this.ftpConnection.ServerAddress;
+            }
+        }
+
+        public string UserName
+        {
+            get
+            {
+                return this.ftpConnection.UserName;
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return this.ftpConnection.Password;
+            }
+        }
+
+        // Открыть соединение
+        public void openConnection()
+        {
+            this.ftpConnection.Connect();
+        }
+
+        // закрыть соединение
+        public void closeConnection()
+        {
+            this.ftpConnection.Close();
+        }
+
+        // получить текущий каталог
+        public string getCurrentDirectory()
+        {
+            return this.ftpConnection.ServerDirectory;
+        }
+
+        // получить список файлов в текущем каталоге
+        public string[] getListFiles()
+        {
+            return this.ftpConnection.GetFiles(this.getCurrentDirectory(), true);
+        }
+
+        // получить список файлов в указанном каталоге
+        public string[] getListFiles(string Directory)
+        {
+            if (Directory.Length < 1)
+                return new string[0];
+
+            return this.ftpConnection.GetFiles(Directory, true);
+        }
+
+        // сменить текущий каталог
+        public bool changeDirectory(string newDirectory)
+        {
+            return this.ftpConnection.ChangeWorkingDirectory(newDirectory);
+        }
+
+        //сменить текущий каталог на родительский
+        public bool changeDirrectoryOnParent()
+        {
+            return this.ftpConnection.ChangeWorkingDirectoryUp();
+        }
+
+        // создать каталог в текущем каталоге
+        public bool createDirectory(string nameNewDirectory)
+        {
+            if (nameNewDirectory.Length < 1)
+                return false;
+            this.ftpConnection.CreateDirectory(nameNewDirectory);
+            return true;
+        }
+
+        //удалить указанный каталог
+        public bool deleteDirectory(string Directory)
+        {
+            if (Directory.Length < 1)
+                return false;
+
+            this.ftpConnection.DeleteDirectory(Directory);
+
+            return true;
+        }
+
+        //удалить указанный файл
+        public bool deleteFile(string remoteFile)
+        {
+            if (remoteFile.Length < 1)
+                return false;
+
+            return this.ftpConnection.DeleteFile(remoteFile);
+        }
+
+        //загрузка файла на сервер в текущую директорию
+        public bool UploadFile(string localFile)
+        {
+            if (localFile.Length < 1)
+                return false;
+
+            string CurrentDirectory = this.getCurrentDirectory();
+            this.ftpConnection.UploadFile(localFile, CurrentDirectory, false); // false - перезаписать файл
+
+            return true;
+        }
+
+        //загрузка файла на сервер в указанную директорию
+        public bool UploadFile(string localFile, string remoteFile)
+        {
+            if (localFile.Length < 1 || remoteFile.Length < 1)
+                return false;
+
+            if (!remoteFile.Contains("/"))
+                remoteFile = this.getCurrentDirectory() + remoteFile;
+
+            this.ftpConnection.UploadFile(localFile, remoteFile, false); // false - перезаписать файл
+            return true;
+        }
+
+        //получение указанного файла с сервера
+        public bool DownloadFile(string localFile, string remoteFile)
+        {
+            if (localFile.Length < 1 || remoteFile.Length < 1)
+                return false;
+
+            if (!remoteFile.Contains("/"))
+                remoteFile = this.getCurrentDirectory() + remoteFile;
+
+            this.ftpConnection.DownloadFile(localFile, remoteFile);
+            return true;
+        } 
+
+        public bool RenameFile(string oldName, string newName)
+        {
+            if (oldName.Length < 1 || newName.Length < 1)
+                return false;
+
+            this.ftpConnection.RenameFile(oldName, newName);
+            return true;
+        }
+    }
+}
--- FtpClient/Program.cs
+++ FtpClient/Program.cs
@@ -3,13 +3,62 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace FtpClient
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
+            while (true)
+            {
+
+            }
+
+
+            Console.ReadLine();
+        }
+
+        public void printMenu()
+        {
+            string strMenu = "";
+            strMenu += "<1> Создать каталог" + Environment.NewLine;
+            strMenu += "<2> Удалить каталог" + Environment.NewLine;
+            strMenu += "<3> Перейти в родительский каталог" + Environment.NewLine;
+            strMenu += "<4> Перейти в указаный каталог" + Environment.NewLine;
+            strMenu += "<5> Показать содержимое текущего каталога" + Environment.NewLine;
+            strMenu += "<6> Получить указаный файл" + Environment.NewLine;
+            strMenu += "<7> Загрузить файл на сервер" + Environment.NewLine;
+            strMenu += "<8> Удалить указаный файл" + Environment.NewLine;
+            strMenu += "<9> Переименовать указаный файл" + Environment.NewLine; 
+            strMenu += "<0> Выход" + Environment.NewLine;
+
+            Console.Write(strMenu);
+        }
+
+        public static bool Upload(string fileName, out string fileNameLocal)
+        {
+            string Name = fileName.Substring(fileName.LastIndexOf('/'));
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Выберите файл ..."; // Заголовок окна
+            ofd.InitialDirectory = Application.StartupPath; // путь откуда запустили
+            //ofd.Filter = "Text documents (*.txt)|*.txt";
+            ofd.Multiselect = false;
+
+            DialogResult result = ofd.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                fileNameLocal = ofd.FileName;
+                return true;
+            }
+            else
+            {
+                fileNameLocal = "";
+                return false;
+            }
         }
     }
 }
